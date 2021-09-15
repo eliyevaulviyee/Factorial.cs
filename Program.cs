74c63018997bf73e278b7236bb3adf3b1e9b3340
eliyevaulviyee <90254085+eliyevaulviyee@users.noreply.github.com>
2021-09-15 22:57:04 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+	public static void Main()
+	{
+	    int factorial=1;
+		Console.Write("Enter number: ");
+		int a=int.Parse(Console.ReadLine());
+        if(a<=0)
+        {
+            Console.WriteLine("An Error Ocurred!");
+            Console.ReadLine();
+        }
+		else
+        {
+            for(int i=1;i<=a;i++)
+            {
+                factorial=factorial*i;
+            }
+            Console.WriteLine(factorial);
+            Console.ReadLine();
+            Console.WriteLine("Factroial Calculated Successfully!");
+            Console.ReadLine();
+        }
+	}
+}
